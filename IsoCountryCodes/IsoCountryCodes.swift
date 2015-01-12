0ad7410612d648c09bc98cf70fbdf69b1a6c521f
Sidney de Koning <sidney@m2mobi.com>
2015-01-12 10:29:21 +0100

    Removed obsolete dependency to UIKit

---
+++ IsoCountryCodes/IsoCountryCodes.swift
@@ -0,0 +1,32 @@
+//
+//  IsoCountryCodes.swift
+//  IsoCountryCodes
+//
+//  Created by Sidney de Koning on 09/01/15.
+//  Copyright (c) 2015 Funky Monkey, www.funky-monkey.nl. All rights reserved.
+//
+
+class IsoCountryCodes {
+    
+    class func find( key:String ) -> IsoCountryInfo {
+        var country = filter( IsoCountries.allCountries, { $0.alpha2 == key.uppercaseString || $0.alpha3 == key.uppercaseString || $0.numeric == key } )
+        return country[0]
+    }
+    
+    class func searchByName( name:String ) -> IsoCountryInfo {
+        var country = filter( IsoCountries.allCountries, { $0.name == name } )
+        
+        return (!country.isEmpty) ? country[0] : IsoCountryInfo(name: "", numeric: "", alpha2: "", alpha3: "", calling: "", currency: "", continent: "")
+    }
+    
+   class func searchByCurrency( currency:String ) -> [IsoCountryInfo] {
+        var country = filter( IsoCountries.allCountries, { $0.currency == currency } )
+        return country
+    }
+    
+    class func searchByCallingCode( calllingCode:String ) -> IsoCountryInfo {
+        var country = filter( IsoCountries.allCountries, { $0.calling == calllingCode } )
+        return country[0]
+    }
+   
+}
