af0622c2cdeb6e25c56ed1faf447c4bad1b43d38
Brian Henry <brian.henry2@gmail.com>
2016-03-11 15:00:17 +0000

    Swift filter() method syntax updated
    After error reported by Xcode: filter is unavailable call the filter()
    method on the sequence

--- IsoCountryCodes.swift
+++ IsoCountryCodes.swift
@@ -9,23 +9,23 @@
 class IsoCountryCodes {
     
     class func find( key:String ) -> IsoCountryInfo {
-        var country = filter( IsoCountries.allCountries, { $0.alpha2 == key.uppercaseString || $0.alpha3 == key.uppercaseString || $0.numeric == key } )
+        var country = IsoCountries.allCountries.filter(  { $0.alpha2 == key.uppercaseString || $0.alpha3 == key.uppercaseString || $0.numeric == key } )
         return country[0]
     }
     
     class func searchByName( name:String ) -> IsoCountryInfo {
-        var country = filter( IsoCountries.allCountries, { $0.name == name } )
+        var country = IsoCountries.allCountries.filter( { $0.name == name } )
         
         return (!country.isEmpty) ? country[0] : IsoCountryInfo(name: "", numeric: "", alpha2: "", alpha3: "", calling: "", currency: "", continent: "")
     }
     
    class func searchByCurrency( currency:String ) -> [IsoCountryInfo] {
-        var country = filter( IsoCountries.allCountries, { $0.currency == currency } )
+        let country = IsoCountries.allCountries.filter(  { $0.currency == currency } )
         return country
     }
     
    class func searchByCallingCode( calllingCode:String ) -> IsoCountryInfo {
-       var country = filter( IsoCountries.allCountries, { $0.calling == calllingCode } )
+       var country = IsoCountries.allCountries.filter( { $0.calling == calllingCode } )
        return country[0]
    }
 }
