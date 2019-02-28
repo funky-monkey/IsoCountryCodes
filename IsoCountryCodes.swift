82b17ded5318b64f9e4ecacf477355e653cfa46f
Marnix Arnold <m.arnold@safened.com>
2019-02-28 13:04:28 +0100

    Made matching by name case- and diacritic-insensitive, and trying partial matching if full match fails

--- IsoCountryCodes.swift
+++ IsoCountryCodes.swift
@@ -15,7 +15,25 @@ class IsoCountryCodes {
     }
 
     class func searchByName(_ name: String) -> IsoCountryInfo? {
-        let countries = IsoCountries.allCountries.filter({ $0.name == name })
+        let options: String.CompareOptions = [.diacriticInsensitive, .caseInsensitive]
+        let name = name.folding(options: options, locale: .current)
+        let countries = IsoCountries.allCountries.filter({ $0.name.folding(options: options, locale: .current) == name })
+        // If we cannot find a full name match, try a partial match
+        return countries.count == 1 ? countries.first : searchByPartialName(name)
+    }
+
+    private class func searchByPartialName(_ name: String) -> IsoCountryInfo? {
+        guard name.count > 3 else {
+            return nil
+        }
+        let options: String.CompareOptions = [.diacriticInsensitive, .caseInsensitive]
+        let name = name.folding(options: options, locale: .current)
+        let countries = IsoCountries.allCountries.filter({ $0.name.folding(options: options, locale: .current).contains(name) })
+        // It is possible that the results are ambiguous, in that case return nothing
+        // (e.g., there are two Koreas and two Congos)
+        guard countries.count == 1 else {
+            return nil
+        }
         return countries.first
     }
 
